9323c80a38301c22cd881d91b05ec172cc481de0
1.kla$ <recum12233@mail.ru>
2024-09-10 22:27:38 +0300

    init

---
+++ src/main/java/org/chiches/foodrootservir/FoodRootServirApplication.java
@@ -0,0 +1,13 @@
+package org.chiches.foodrootservir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodRootServirApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodRootServirApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/chiches/foodrootservir/FoodRootServirApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chiches.foodrootservir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodRootServirApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
