8d9ed9509cab56a6de1d97577f5e5e87a5808328
1.kla$ <recum12233@mail.ru>
2024-10-14 20:47:00 +0300

    Abubs -cors enabled for everything possible -temporary disabled authentication

--- src/main/java/org/chiches/foodrootservir/repositories/RefreshTokenRepository.java
+++ src/main/java/org/chiches/foodrootservir/repositories/RefreshTokenRepository.java
@@ -1,9 +1,10 @@
 package org.chiches.foodrootservir.repositories;
 
 import org.chiches.foodrootservir.entities.RefreshTokenEntity;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface RefreshTokenRepository extends GeneralRepository<RefreshTokenEntity, Long> {
     void delete(RefreshTokenEntity refreshToken);
     int deleteByUserId(Long userId);
