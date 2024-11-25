732e7e1c8d32a17a0a53079e2c9be1217619e426
1.kla$ <recum12233@mail.ru>
2024-11-25 23:18:54 +0300

    ful corsus

--- src/main/java/org/chiches/foodrootservir/misc/CookieUtil.java
+++ src/main/java/org/chiches/foodrootservir/misc/CookieUtil.java
@@ -8,34 +8,40 @@ import org.springframework.stereotype.Service;
 public class CookieUtil {
     public static final String JWT_COOKIE_NAME = "jwtToken";
     public static final String REFRESH_COOKIE_NAME = "refreshToken";
+    public static final String DOMAIN = "localhost";
+    public static final String PATH = "/api";
     private static final boolean HTTP_ONLY_JWT = false;// FIXME: security issue, XSS vulnerability
     private static final boolean HTTP_ONLY_REFRESH = true;
     public Cookie createJWTCookie(String jwt) {
         Cookie cookie = new Cookie(JWT_COOKIE_NAME, "Bearer_" + jwt);
         cookie.setHttpOnly(HTTP_ONLY_JWT);
-        cookie.setPath("/api");
+        cookie.setPath(PATH);
         cookie.setMaxAge(60 * 30);
+        cookie.setDomain(DOMAIN);
         return cookie;
     }
     public Cookie createRefreshCookie(String refreshToken) {
         Cookie cookie = new Cookie(REFRESH_COOKIE_NAME, refreshToken);
         cookie.setHttpOnly(HTTP_ONLY_REFRESH);
-        cookie.setPath("/api");
+        cookie.setPath(PATH);
         cookie.setMaxAge(86400);
+        cookie.setDomain(DOMAIN);
         return cookie;
     }
     public Cookie deleteJWTCookie() {
         Cookie cookie = new Cookie(JWT_COOKIE_NAME, "");
         cookie.setHttpOnly(HTTP_ONLY_JWT);
-        cookie.setPath("/api");
+        cookie.setPath(PATH);
         cookie.setMaxAge(0);
+        cookie.setDomain(DOMAIN);
         return cookie;
     }
     public Cookie deleteRefreshCookie() {
         Cookie cookie = new Cookie(REFRESH_COOKIE_NAME, "");
         cookie.setHttpOnly(HTTP_ONLY_REFRESH);
-        cookie.setPath("/api");
+        cookie.setPath(PATH);
         cookie.setMaxAge(0);
+        cookie.setDomain(DOMAIN);
         return cookie;
     }
     public void logout(HttpServletResponse response) {
