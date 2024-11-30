f2142d2087f48918a8dc6c8da04c23a11541062d
macostacos <ivanov.maxim0@yandex.ru>
2024-11-30 18:18:27 +0300

    feat: added pagination functionality for dishes

--- src/main/java/org/chiches/foodrootservir/repositories/DishItemRepository.java
+++ src/main/java/org/chiches/foodrootservir/repositories/DishItemRepository.java
@@ -2,6 +2,8 @@ package org.chiches.foodrootservir.repositories;
 
 import org.chiches.foodrootservir.entities.CategoryEntity;
 import org.chiches.foodrootservir.entities.DishItemEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,7 +14,7 @@ import java.util.Optional;
 @Repository
 public interface DishItemRepository extends GeneralRepository<DishItemEntity, Long> {
 
-    List<DishItemEntity> findAllByCategoryAndIsDeletedFalse(CategoryEntity categoryEntity);
+    Page<DishItemEntity> findAllByCategoryAndIsDeletedFalse(CategoryEntity categoryEntity, Pageable pageable);
 
     List<DishItemEntity> findAllByNameContainingIgnoreCaseAndIsDeletedFalse(String name);
 
